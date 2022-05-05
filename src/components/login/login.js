39f8c2f4531f05e5b5970a7bba478a08a377fb5a
Gerardo Santa Cruz <gsantacruz@mvfactory.com>
2022-05-05 19:34:22 -0300

    Update Login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,42 +1,48 @@
+import { useFormInput } from '../utils/forms';
+import { URL } from '../../constant';
 import './login.css';
 
 function Login(props){
+    
+    const username = useFormInput('');
+    const password = useFormInput('');
+
     function handleSubmit(){
 
-        const options= {
-            method: 'POST',
+        let jsonData = {
+            "username": username.value,
+            "password": password.value
+        };
+
+        const options = {
+            method: "POST",
             headers: {
-                "Content-Type": "application/json",
+            "Content-Type": "application/json"
             },
             body: JSON.stringify(jsonData)
-        }
+        };
 
-        fetch('https://jsonplaceholder.typicode.com/users/2', options)
-        .then(response => response.json())
-        .then(json => {
-           console.log(json); 
-        });
+        fetch(URL, options)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+            });     
     }
 
-    return(
+    return (
         <div className="login">
             <div className="login-header">
                 <div className="login-header-logo">
-                    <div>
-                        <label>Username</label>
-                        <input></input>
-                    </div>
-                    <div>
-                        <label>Password</label>
-                        <input type="password"></input>
-                    </div>
-                    <div>
-                        <button onClick={console.log('Llamada a donde...')}>Login</button>
-                    </div>
+                    <label>Username</label>
+                    <input {...username}></input><br></br>
+                    <label>Password</label>
+                    <input type="password" {...password}></input><br></br>
+                    <button onClick={handleSubmit}>Login</button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
+
 
 export default Login;
